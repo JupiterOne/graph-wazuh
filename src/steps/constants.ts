6ba8e5518f4f6039ddb104810f22fc496a8124ac
Austin Kelleher <austinlkelleher@gmail.com>
2022-04-22 10:25:38 -0400

    INT-3466 - Replace with integration-template code

---
+++ src/steps/constants.ts
@@ -0,0 +1,10 @@
+import {
+  StepEntityMetadata,
+  StepRelationshipMetadata,
+} from '@jupiterone/integration-sdk-core';
+
+export const Steps = {};
+
+export const Entities: Record<string, StepEntityMetadata> = {};
+
+export const Relationships: Record<string, StepRelationshipMetadata> = {};
---
+++ src/steps/index.ts
@@ -0,0 +1,3 @@
+const integrationSteps = [];
+
+export { integrationSteps };
